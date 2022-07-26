77b304f6bd51a41e0d89a6c1b392c50a82baa7e8
URK21CO2013 <110015085+URK21CO2013@users.noreply.github.com>
2022-07-26 23:13:23 +0530

    initial commit

--- Main.py
+++ Main.py
@@ -37,7 +37,7 @@ size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
 for line in range(queries):
     values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
+if values[0] == 1:
         stack.push(values[1])
     elif values[0] == 2:
         stack.pop()
